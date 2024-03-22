713a69cf15a38231cc294e85caee9b0a7db2d42a
NeidjaRayssa <143639965+NeidjaRayssa@users.noreply.github.com>
2024-03-22 16:08:35 -0300

    Add files via upload

---
+++ Troca.js
@@ -0,0 +1,11 @@
+const title = document.getElementById('h1');
+let genero = document.getElementsByClassName('um');
+const caixaPrincipal = document.getElementById('container');
+const frevo = document.getElementById('frevo');
+
+frevo.addEventListener('click',
+
+function(){
+        caixaPrincipal.style.display = 'none'
+}
+)
